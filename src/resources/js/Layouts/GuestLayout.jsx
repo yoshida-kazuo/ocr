9543521a110dcdd6232244b101643b23ec33d3a9
Ubuntu <ubuntu@dev.gsingularity.com>
2023-12-22 13:11:35 +0000

    dockerポート調整

--- src/resources/js/Layouts/GuestLayout.jsx
+++ src/resources/js/Layouts/GuestLayout.jsx
@@ -6,6 +6,12 @@ import LangSelector from '@/Components/LangSelector';
 export default function Guest({ children, lang, timezone }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+            <div>
+                <Link href="/">
+                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                </Link>
+            </div>
+
             <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {children}
             </div>

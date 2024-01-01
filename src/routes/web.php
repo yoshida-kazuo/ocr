8739070bcef2190cc79bfd611231513323c30d17
KazuoYoshida <yoshida@vril.co.jp>
2024-01-01 15:39:40 +0900

    route調整

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,10 +5,11 @@
 | Web Routes
 |--------------------------------------------------------------------------
 */
-require __DIR__.'/web-guest.php';
 
-require __DIR__.'/web-user.php';
-require __DIR__.'/web-admin.php';
-require __DIR__.'/web-root.php';
+require __DIR__.'/web/guest.php';
+
+require __DIR__.'/web/user.php';
+require __DIR__.'/web/admin.php';
+require __DIR__.'/web/root.php';
 
 require __DIR__.'/auth.php';

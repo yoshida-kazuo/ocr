4214afa30e2ca8a740108b5067c0b397b1675c60
KazuoYoshida <yoshida@vril.co.jp>
2024-01-07 17:27:52 +0900

    google認証バグ修正

--- src/app/Http/Controllers/V1/Web/Auth/Google/CallbackController.php
+++ src/app/Http/Controllers/V1/Web/Auth/Google/CallbackController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers\V1\Web\Auth\Google;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Validator;
 use Laravel\Socialite\Facades\Socialite;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Throwable;
 
 class CallbackController extends Controller
 {
@@ -25,38 +25,43 @@ class CallbackController extends Controller
         $googleUser = Socialite::driver('google')
             ->user();
 
-        $validator = Validator::make([
-            'google_auth' => $googleUser->getEmail(),
-        ], [
-            'google_auth' => [
-                'unique:App\Models\User,email',
-            ],
-        ], [
-            'google_auth.unique'  => __('auth.failed'),
-        ]);
+        try {
+            $user = User::firstOrCreate([
+                'email'             => $googleUser->getEmail()
+            ], [
+                'name'              => $googleUser->getName(),
+                'password'          => 'no-login-' . Str::random(60),
+                'email_verified_at' => now(),
+            ]);
 
-        if ($validator->fails()) {
-            activity(__(':id : :email : Google authentication failed. The email is already registered.', [
+            if ($user->wasRecentlyCreated) {
+                $user->refresh();
+            }
+        } catch(\Illuminate\Database\UniqueConstraintViolationException $e) {
+            report($e);
+            activity(
+                __(':id : :email : Google authentication failed. The email is already registered.', [
                     'id'    => $googleUser->id,
                     'name'  => $googleUser->getName(),
                     'email' => $googleUser->getEmail(),
                 ]),
-                'info');
+                'info'
+            );
 
             return to_route('login')
-                ->withErrors($validator);
-        }
-
-        $user = User::firstOrCreate([
-            'email'             => $googleUser->getEmail()
-        ], [
-            'name'              => $googleUser->getName(),
-            'password'          => 'no-login-' . Str::random(60),
-            'email_verified_at' => now(),
-        ]);
-
-        if ($user->wasRecentlyCreated) {
-            $user->refresh();
+                ->withErrors([
+                    'google_auth'   => __('auth.failed'),
+                ]);
+        } catch(Throwable $e) {
+            report($e);
+            activity(
+                __(':id : :email : Google authentication failed.', [
+                    'id'    => $googleUser->id,
+                    'name'  => $googleUser->getName(),
+                    'email' => $googleUser->getEmail(),
+                ]),
+                'info'
+            );
         }
 
         Auth::login($user);

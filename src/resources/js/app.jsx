d27c81ea80f865ec9d788cbe931ca36b761c05c5
KazuoYoshida <yoshida@vril.co.jp>
2024-01-26 09:28:26 +0900

    ssr
    ssr

--- src/resources/js/app.jsx
+++ src/resources/js/app.jsx
@@ -1,7 +1,6 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import './bootstrap';
 import '../css/app.scss';
 import.meta.glob([
     '../images/**',
@@ -12,11 +11,14 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) => resolvePageComponent(
+        `./Pages/${name}.jsx`,
+        import.meta.glob('./Pages/**/*.jsx')
+    ),
     setup({ el, App, props }) {
-        const root = createRoot(el);
+        //
 
-        root.render(<App {...props} />);
+        hydrateRoot(el, <App {...props} />);
     },
     progress: {
         delay: 250,

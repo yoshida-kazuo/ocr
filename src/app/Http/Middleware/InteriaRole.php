78cbaec4c5c15a1b10c37eafc94eac93e9adcb3a
KazuoYoshida <yoshida@vril.co.jp>
2024-02-02 09:17:37 +0900

    roleview統一

--- src/app/Http/Middleware/InteriaRole.php
+++ src/app/Http/Middleware/InteriaRole.php
@@ -56,15 +56,7 @@ class InteriaRole extends HandleInertiaRequests
             ->get(user('role_id'));
 
         if (is_string($roleName)) {
-            $rootView = "{$rootView}-{$roleName}";
-
-            Inertia::share(
-                'isProvider',
-                ! (bool) Str::afterLast(
-                    user('email'),
-                    config('app.user_dummy_email_domain')
-                )
-            );
+            // $rootView = "{$rootView}-{$roleName}";
         }
 
         return $rootView;
